74f52cca2145d4217c0e405b1c3e0d18cb74c88e
PodgeCollins <collinspadraic97@gmail.com>
2021-03-12 14:12:50 +0000

    Add functions to scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,7 +1,52 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+
 function flipCard(){
-    this.classList.toggle('flip')
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        // first click
+    } else{
+        hasFlippedCard = false;
+        secondCard = this;
+        // second click
+        
+        checkForMatch();
+    }
 }
+
+function checkForMatch(){
+    // do cards march?
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+            // its a match
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+            disableCards();
+            
+        } else {
+            unFlipCards();
+        }
+}
+
+function disableCards(){
+            // its a match
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+            // cards cant be flipped back upon clicking
+}
+
+function unFlipCards(){
+            // not a match
+            setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+            }, 1500);
+}
+
 
 cards.forEach(card=>card.addEventListener('click', flipCard));
